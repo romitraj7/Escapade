96ffbadd7934a8b9295f1b78441fa1913b76ab4d
Romit <romit2884@gmail.com>
2024-02-11 19:17:05 +0530

    create pages and components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import FooterComponent from './components/FooterComponent';
+import Home from './pages/Home';
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <FooterComponent />
+    </BrowserRouter>
+  );
 }
