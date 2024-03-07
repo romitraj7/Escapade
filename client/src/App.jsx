b7af246f5f17ecc428b3116470dc789a4656108e
Romit <romit2884@gmail.com>
2024-03-07 22:47:41 +0530

    add scroll to top component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import FooterComponent from './components/FooterComponent';
 import Home from './pages/Home';
 import Events from './pages/Events.jsx';
 import EventDetails from './pages/EventDetails/EventDetails.jsx';
+import ScrollToTop from './components/ScrollToTop.jsx';
 
 export default function App() {
   return (
     <BrowserRouter>
+    <ScrollToTop/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/events' element={<Events />} />
