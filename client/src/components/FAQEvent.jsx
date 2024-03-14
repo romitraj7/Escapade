08c81719eece9789c8a63ca928d6fd80f60a9271
Romit <romit2884@gmail.com>
2024-03-14 19:58:26 +0530

    updating FAQ

--- client/src/components/FAQEvent.jsx
+++ client/src/components/FAQEvent.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import {Accordion, AccordionItem} from "@nextui-org/react";
 export default function FAQEvent() {
     const question1 =
-    "Answer1.";
+    "Refreshments will be provided to all the teams who qualify for the 2nd round.";
     const question2 =
-    "Answer2.";
+    "The participants certificate from this event can be used to avail MAR points which are mandatory for every student to submit at the end of an year.";
     const question3 =
     "Answer3";
   return (
@@ -13,15 +13,15 @@ export default function FAQEvent() {
          <h3 className="flex justify-center font-bold mt-4 font-luckyfarmer text-4xl md:text-5xl underline">Frequently Asked Questions?</h3>       
       </div>
         <Accordion variant="splitted" className='p-2 flex flex-col gap-1 max-w-[500px]'>
-      <AccordionItem key="1" aria-label="Accordion 1" title="Question1?">
+      <AccordionItem key="1" aria-label="Accordion 1" title="What are the perks of this event?">
         {question1}
       </AccordionItem>
-      <AccordionItem key="2" aria-label="Accordion 2" title="Question2?">
+      <AccordionItem key="2" aria-label="Accordion 2" title="How can the participant certificate be used?">
         {question2}
       </AccordionItem>
-      <AccordionItem key="3" aria-label="Accordion 3" title="Question3?">
+      {/* <AccordionItem key="3" aria-label="Accordion 3" title="How can this even?">
         {question3}
-      </AccordionItem>
+      </AccordionItem> */}
         </Accordion>
     </div>
   )
